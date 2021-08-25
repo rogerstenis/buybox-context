b2ee7bef03cab7a488a3c546f725b7501a12bf7f
Wagner Lemos <wagner.duarte@vtex.com.br>
2021-08-25 10:07:30 -0300

    Sort sellers by price

---
+++ react/hooks/useReduceProduct.ts
@@ -0,0 +1,38 @@
+import { useCallback } from 'react'
+import {
+  Item,
+  ProductContextState,
+} from 'vtex.product-context/react/ProductTypes'
+
+interface ReducerInterface {
+  productItemsWithoutSelected: Item[]
+  productSelectedItem: Item | undefined
+}
+
+const initialValue: ReducerInterface = {
+  productItemsWithoutSelected: [],
+  productSelectedItem: undefined,
+}
+
+export const useReduceProduct = (
+  productContext: Partial<ProductContextState>
+): ReducerInterface => {
+  const reducer = useCallback(
+    (acumulator: ReducerInterface, currentValue: Item) => {
+      if (currentValue.itemId === productContext.selectedItem?.itemId) {
+        acumulator.productSelectedItem = { ...currentValue }
+      } else {
+        acumulator.productItemsWithoutSelected?.push({ ...currentValue })
+      }
+
+      return acumulator
+    },
+    [productContext.selectedItem?.itemId]
+  )
+
+  if (!productContext.product) {
+    return initialValue
+  }
+
+  return productContext.product.items.reduce(reducer, initialValue)
+}
