5e3afe14fd2118d628b1389be56713c9ad7469a5
Wagner Lemos <wagner.duarte@vtex.com.br>
2021-09-07 16:11:16 -0300

    Create hook useNewProductWithSellers

---
+++ react/hooks/useNewProductWithSellers.ts
@@ -0,0 +1,52 @@
+import { useMemo } from 'react'
+import {
+  MaybeProduct,
+  ProductContextState,
+} from 'vtex.product-context/react/ProductTypes'
+
+import { Seller } from '../typings/types'
+import { useReduceProduct } from './useReduceProduct'
+
+interface Props {
+  productContext: Partial<ProductContextState>
+  sellers: Seller[]
+}
+
+// ToDo: criar testes
+export const useNewProductWithSellers = ({
+  productContext,
+  sellers,
+}: Props): MaybeProduct => {
+  const { productSelectedItem, productItemsWithoutSelected } = useReduceProduct(
+    productContext
+  )
+
+  const productWithSortedSellers = useMemo<MaybeProduct>(() => {
+    if (
+      productContext.product &&
+      productSelectedItem &&
+      productItemsWithoutSelected
+    ) {
+      return {
+        ...productContext.product,
+        items: [
+          ...productItemsWithoutSelected,
+          { ...productSelectedItem, sellers: [...sellers] },
+        ],
+      }
+    }
+
+    return undefined
+  }, [
+    productContext.product,
+    productSelectedItem,
+    productItemsWithoutSelected,
+    sellers,
+  ])
+
+  const newProduct = useMemo(() => {
+    return productWithSortedSellers ?? productContext.product
+  }, [productContext.product, productWithSortedSellers])
+
+  return newProduct
+}
